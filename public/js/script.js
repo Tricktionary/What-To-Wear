b8078e690733c5463d900c1070d830e3b325b92d
Tricktionary <andyaoux@hotmail.com>
2017-11-13 19:49:43 -0500

    Icon Dynamic

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,16 @@
+//Weather Icon Json
+var weatherIcons;
+
+$(document).ready(function(){
+
+    //load in JSON object
+    $.getJSON("weatherIcon.json", function(json) {
+       weatherIcons = json;
+    });
+});
+
+
+//Get the current weather conditions
 function getWeather() {
 
     var cityName = $('#city').val();
@@ -7,29 +20,39 @@ function getWeather() {
 
     $.get(`/weather?city=${cityName}`,function(data){
         
+        var prefix = 'wi wi-';
         var obj = JSON.parse(data);
         console.log(obj);
         
-        var temp = (obj.main.temp-273).toFixed(2);
+        //Current Temperature
+        var temp = Math.round(obj.main.temp-273);
+
+        //Main Weather Condition
         var mainCond = obj.weather[0].main;
 
-        $("#weatherDesc").text("Description: "+obj.weather[0].description);
-        $("#temp").text("Temperature in Celcius: " + temp);
+        //Weather Icon
+        var code = obj.weather[0].id;
+        var icon = weatherIcons[code].icon;
 
+        if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+            icon = 'day-' + icon;
+        }
+        icon = prefix + icon;
+        console.log(icon);
+
+        $("#weatherDesc").text("Description: "+obj.weather[0].description);
+        $("#currTemp").text("Current temperature: " + temp +"°C");
+        $("#highTemp").text("High of: "+ Math.round(obj.main.temp_max - 273) +"°C");
+        $("#lowTemp").text("Low of: "+ Math.round(obj.main.temp_min - 273) +"°C");
+        $("#weatherIcon")[0].classList = "";
+        $("#weatherIcon")[0].classList = icon;
         setClothing(temp,mainCond);
     });
 
-    //Attach Enter-key Handler
-    const ENTER=13
-    document.getElementById("city")
-        .addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.keyCode === ENTER) {
-            document.getElementById("submit").click();
-        }
-    });
+
 }
 
+//Set the clothing
 function setClothing(temp,condition){
     var head  = $("#head");
     var uBody = $("#uBody");
